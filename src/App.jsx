fe6bca7b2de2ce58dd9f0a4dbf8c7334ec44baba
salmosawi <hasonx2005@gmail.com>
2025-04-14 19:52:35 +0300

    feat: handle not found pages and route properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/login.jsx";
 import Dashboard from "./pages/dashboard.jsx";
+import NotFoundPage from "./pages/notFound.jsx";
 import './assets/css/global.css'; // Adjust the path based on where the CSS file is located
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/graphql/login/" element={<LoginPage />} />
+        <Route path="/graphql/dashboard/" element={<Dashboard />} />
+        <Route path="*" element={<NotFoundPage />} /> {/* Handles all other paths */}
       </Routes>
     </Router>
   );
