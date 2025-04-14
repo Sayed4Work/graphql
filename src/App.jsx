574b1fbae9119c2f3683ac0709dff7172d9b85a8
salmosawi <hasonx2005@gmail.com>
2025-04-15 02:28:11 +0300

    fix: no import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/login.jsx";
 import Dashboard from "./pages/dashboard.jsx";
 import NotFoundPage from "./pages/notFound.jsx";
